88f0e6581f0797b3205ce9ef01d10d87796bf94c
Fezile Nkuna <fezile_nkuna@hotmail.com>
2024-05-05 13:05:51 +0200

    Lines were too short to be broken up; I was deceived by VIM

--- alac2flac.sh
+++ alac2flac.sh
@@ -1,12 +1,10 @@
 #! /usr/bin/zsh
 
 # Convert Apple Lossless Audio Codec (ALAC) to Free Lossless Audio Codec (FLAC).
-#
-# This script assumes the ALAC files are in one directory
-# and, therefore, takes no arguments.
 
-# It is the user's duty to confirm that the
-# M4A contains an ALAC file, not AAC.
+# This script assumes the ALAC files are in one directory and, therefore, takes no arguments.
+
+# It is the user's duty to confirm that the M4A contains an ALAC file, not AAC.
 # In most cases, lossy-to-lossless compression is not ideal.
 
 for track in *m4a
