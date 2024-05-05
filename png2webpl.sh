8046eac590bca8564aa599e1ca4991a33313415f
Fezile Nkuna <fezile_nkuna@hotmail.com>
2024-05-05 05:58:43 +0200

    Added png2webpl and ewnameFLACfile

---
+++ png2webpl.sh
@@ -0,0 +1,48 @@
+#! /usr/bin/sh
+
+# Convert 8-bit PNG files to lossless WebP.
+
+png="$1"
+argc=$#
+webp="$(echo $png|awk '{gsub(".png", ".webp"); print}')"
+
+if [ $argc -lt 1 ]
+then
+	echo "Usage: \n\t$0 [PNG file]"
+	exit 1
+fi
+
+bitdepth=$(exiftool "$png"|grep -i "Bit Depth"|cut -f 2 -d ":"|cut -d " " -f 2)
+# WebP does not support colour depths greater than 8.
+if [ $bitdepth -gt 8 ]
+then
+	echo "'$png' not converted: Bit depth not supported. " > /dev/stderr
+	# Consider converting your 16-bit PNGs to JPEG XL (which incorporated FLIF, the Free Lossless Image Format).
+	exit 1
+fi
+
+# Use ImageMagick
+convert "$png" -define webp:lossless=true "$webp" && echo -n "Losslessly converted '$png' to '$webp'"
+# Since I ignore the digits after ".", the result is a floor.
+# bc: Changing the scale, to reduce the number of digits after ".", massively affects the accuracy.
+# I have to read more documentation, but this will do for now.
+echo "($(echo "100 * (1 - $(du -b "$webp"|cut -d "	" -f 1) / $(du -b "$png"|cut -d "	" -f 1))"|bc -l|cut -d "." -f1)% smaller)"
+
+# Follow the psedocode below to check if the generated WebP is truly lossless.
+# ffmpeg -i [PNG file] fromPNG.ppm
+# ffmpeg -i [WebP file] fromWebP.ppm
+# if checksum(fromPNG.ppm) == checksum(fromWebP.ppm) # checksum can be crc32, md5sum, sha1sum, etc.
+# 	WebP is lossless
+# else WebP not lossless
+# I chose the PPM format because it does not store metadata, which can affect the checksum.
+
+# As far as I know, there is no de-facto standard on how/where to store metadata/tags in PNGs.
+# Though ImageMagick keeps the tags during the conversion,
+# it is possible that some tags will be lost.
+# An alternative solution would be exporting, stripping, and importing the tags with exiftool.
+#
+# If, like me, you have thousands of screenshots spanning a decade,
+# you can convert all of them with the command 'find -type f -iname  "*png" -exec png2webpl \{\} \;'
+# and free approximately 50% of the space taken by the PNGs, without any quality loss.
+# Using the find command and deleting the PNGs is safe, since, in general, screenshots have an 8-bit colour depth,
+# have no metadata and use the sRGB colour space (which need not be embedded in the file).
---
+++ renameFLACfile.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/zsh
+
+for song in *flac
+do
+	# File systems in UNIX-like systems disallow the forward slash (/), as do DOS-like systems the colon (:) and other characters.
+	# The AWK command substitues / (ASCII: 0x2f) with ⧸ (UTF8: 0xe2a7b0).
+	title=$(metaflac --show-tag=title "$song"|cut -d "=" -f2|awk '{gsub("/", "⧸"); print}')
+	
+	tracknumber=$(metaflac --export-tags-to=- "$song"|egrep -i "track=|tracknumber="|cut -d "=" -f2)
+	tracktotal=$(metaflac --export-tags-to=- "$song"|egrep -i "tracktotal|totaltracks="|cut -d "=" -f2)
+	
+	# Do not hide the file if the tracknumber tag does not exist (". Hello.flac").
+	if [ -z $tracknumber ]
+	then
+		if [ -z "$title" ]
+		then
+			echo ""$song" does not have a TITLE tag. Skipping..." > /dev/stderr
+			continue
+		else
+			echo ""$song" will be renamed without a TRACKNUMBER / TRACK tag." > /dev/stderr
+			mv -nv "$song" "$title"".flac"
+			continue
+		fi
+	fi
+	
+	if [ "$tracktotal" -gt 9 -a "$tracknumber" -lt 10 ]
+		then
+			newfilename="0""$tracknumber"". ""$title"".flac"	
+			mv -nv "$song" "$newfilename"
+		else
+			newfilename="$tracknumber"". ""$title"".flac"
+			mv -nv "$song" "$newfilename"
+	fi
+done
