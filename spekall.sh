88573bfb0b387427591941d0c7bddb1591fa3fa3
Fezile Nkuna <fezile_nkuna@hotmail.com>
2024-05-29 16:38:44 +0200

    Script now works with Zsh, Bash and the Borne shell (sh). POSIX-compliant?

--- spekall.sh
+++ spekall.sh
@@ -1,28 +1,36 @@
-#! /usr/bin/zsh 
-# For some reason this script doesn't work with bash, but I honestly don't care because zsh is my favourite shell anyway
+#! /usr/bin/sh 
 
 numberOfCheckedFiles=1
 numberOfFilesToBeChecked=0
-for file in *; do
-	file -b $file|egrep -i "audio|mp4|mpeg|flac|wav|m4a|aac|aiff|media|matroska|3gp|opus|wmv" &> /dev/null
-	if [[ $? -eq 0 ]]; then
+
+for audiofile in *
+do
+	file -b "$audiofile"|egrep -i "audio|mp4|mpeg|flac|wav|m4a|aac|aiff|media|matroska|3gp|opus|wmv" > /dev/null
+	isAudio=$?
+	if [ $isAudio -eq 0 ]
+	then
 		numberOfFilesToBeChecked=$((numberOfFilesToBeChecked + 1))
 	fi
 done
 
-if [[ $numberOfFilesToBeChecked -eq 0 ]]; then
+if [ $numberOfFilesToBeChecked -eq 0 ]
+then
 	echo "There are no files with audio streams in this directory"
 	exit 1 
 fi
 
-for file in *; do
-	file -b $file|egrep -i "audio|mp4|mpeg|flac|wav|m4a|aac|aiff|media|matroska|3gp|opus|wmv" &> /dev/null 
+for audiofile in *
+do
 	# "audio" should cover roughly all files 
+	file -b "$audiofile"|egrep -i "audio|mp4|mpeg|flac|wav|m4a|aac|aiff|media|matroska|3gp|opus|wmv" > /dev/null 
 	 
-	isAudio=$? # Get the return status of the last command. If it's non-zero, indicating that the expression was not matched in the search, skip the file.
-	if [[ isAudio -eq 0 ]]; then
-		echo "$numberOfCheckedFiles/$numberOfFilesToBeChecked. Running Spek on '$file'" 1>&1
-		spek $file 2> /dev/null # OPTIONAL AND NOT RECOMMENDED: Append an ampersand (&) hereto to perform all checks concurrently 
+	# Get the return status of the last command.
+	# If it's non-zero, indicating that the expression was not matched in the search, skip the file.
+	isAudio=$?
+	if [ $isAudio -eq 0 ]
+	then
+		echo "$numberOfCheckedFiles/$numberOfFilesToBeChecked. spek '$audiofile'"
+		spek "$audiofile" 2> /dev/null # OPTIONAL AND NOT RECOMMENDED: Append an ampersand (&) hereto to perform all checks concurrently 
 		numberOfCheckedFiles=$((numberOfCheckedFiles + 1))
 	fi
 done
